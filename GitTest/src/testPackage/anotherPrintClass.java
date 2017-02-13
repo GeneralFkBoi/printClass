de622a37f8b6d54262024d201ae24107701c5a93
Reindeersen <Reindeersen@DESKTOP-BVH2ED4>
2017-02-13 22:45:29 +0100

    This is my second update. Are things coming along just fine?

--- GitTest/src/testPackage/anotherPrintClass.java
+++ GitTest/src/testPackage/anotherPrintClass.java
@@ -6,5 +6,8 @@ public class anotherPrintClass {
 		System.out.println("Swag.");
 		
 		System.out.println("Hello.");
+		int a = 69;
+		int b = 420;
+		System.out.println("Lmao " + a + ":" + b);
 	}
 }
